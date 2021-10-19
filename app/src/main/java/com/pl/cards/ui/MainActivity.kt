b93f303fac4ef404a5793969f1a5c180b24c964c
deetree <49595495+deetree@users.noreply.github.com>
2021-10-19 16:16:39 +0200

    Added stores insert on app first run and/or update

--- app/src/main/java/com/pl/cards/ui/MainActivity.kt
+++ app/src/main/java/com/pl/cards/ui/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.pl.cards.ui
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.security.keystore.KeyGenParameterSpec
 import android.view.View
 import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textview.MaterialTextView
+import com.pl.cards.BuildConfig
 import com.pl.cards.R
 import com.pl.cards.helper.StoresTemplate
 import com.pl.cards.ui.adapter.GridAdapter
@@ -32,8 +33,6 @@ class MainActivity : AppCompatActivity() {
 
         grid.isNestedScrollingEnabled = true
 
-        storesList.forEach { s -> storeViewModel.insert(s) }
-
         val adapter = GridAdapter(this, emptyList())
         grid.adapter = adapter
 
@@ -47,9 +46,33 @@ class MainActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
 
+        if (isAfterUpdate())
+            storesList.forEach { s -> storeViewModel.insert(s) }
+
         add.setOnClickListener {
             startActivity(Intent(this, AddCardActivity::class.java))
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+
+    private fun isAfterUpdate(): Boolean {
+        val VERSION_CODE = "VERSION_CODE"
+        val DOESNT_EXIST = -1
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+
+        val savedVersion = sharedPref.getInt(VERSION_CODE, DOESNT_EXIST)
+        val currentVersion = BuildConfig.VERSION_CODE
+
+        /*currentVersion == savedVersion -> return false
+        savedVersion == DOESNT_EXIST -> save & return true
+        currentVersion > savedVersion -> save & return true*/
+
+        return if (currentVersion == savedVersion)
+            false
+        else {
+            sharedPref.edit().putInt(VERSION_CODE, currentVersion).apply()
+            true
         }
     }
 }
